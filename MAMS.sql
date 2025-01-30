ae5f7126fe190a27dfe1239ece44f5270992fee2
Your Name <your.email@example.com>
2025-01-30 16:54:56 +0100

    changes

--- MAMS.sql
+++ MAMS.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : ven. 24 jan. 2025 à 12:03
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
+-- Hôte : localhost
+-- Généré le : jeu. 30 jan. 2025 à 15:54
+-- Version du serveur : 10.11.10-MariaDB
+-- Version de PHP : 8.3.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `projetjava`
+-- Base de données : `MAMS`
 --
 
 -- --------------------------------------------------------
@@ -29,7 +29,6 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `administrator` (
   `id` bigint(20) NOT NULL,
-  `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `firstName` varchar(255) DEFAULT NULL,
@@ -40,8 +39,8 @@ CREATE TABLE `administrator` (
 -- Déchargement des données de la table `administrator`
 --
 
-INSERT INTO `administrator` (`id`, `username`, `password`, `email`, `firstName`, `lastName`) VALUES
-(1, 'admin', 'adminpassword', 'admin@example.com', 'Admin', 'User');
+INSERT INTO `administrator` (`id`, `password`, `email`, `firstName`, `lastName`) VALUES
+(1, 'adminpassword', 'admin@example.com', 'Admin', 'User');
 
 -- --------------------------------------------------------
 
@@ -62,9 +61,16 @@ CREATE TABLE `appointment` (
 --
 
 INSERT INTO `appointment` (`id`, `dateTime`, `status`, `doctor_id`, `patient_id`) VALUES
-(1, '2025-01-24 10:00:00', 'Scheduled', 1, 1),
-(2, '2025-01-24 10:25:18', 'SCHEDULED', 1, 1),
-(3, '2025-01-24 10:26:59', 'SCHEDULED', 1, 1);
+(10, '2025-01-30 15:21:34', 'SCHEDULED', 19, 4),
+(11, '2025-01-30 15:23:40', 'SCHEDULED', 19, 4),
+(12, '2025-01-30 15:28:43', 'SCHEDULED', 19, 4),
+(13, '2025-01-30 15:31:15', 'SCHEDULED', 19, 4),
+(14, '2025-01-30 15:32:31', 'SCHEDULED', 19, 5),
+(15, '2025-01-30 16:13:04', 'SCHEDULED', 19, 5),
+(16, '2025-01-30 16:16:58', 'SCHEDULED', 19, 5),
+(17, '2025-01-30 16:18:17', 'SCHEDULED', 19, 5),
+(18, '2025-01-30 16:23:47', 'SCHEDULED', 19, 5),
+(19, '2025-01-30 16:24:09', 'SCHEDULED', 19, 5);
 
 -- --------------------------------------------------------
 
@@ -83,7 +89,11 @@ CREATE TABLE `department` (
 --
 
 INSERT INTO `department` (`id`, `name`, `description`) VALUES
-(1, 'Cardiology', 'Heart related medical treatments and procedures.');
+(1, 'Cardiology', 'Heart related medical treatments and procedures.'),
+(2, 'Neurology', 'Focuses on disorders of the nervous system.'),
+(3, 'Pediatrics', 'Provides medical care for children.'),
+(4, 'Orthopedics', 'Deals with bones, joints, and muscles.'),
+(5, 'Dermatology', 'Specializes in skin diseases and treatments.');
 
 -- --------------------------------------------------------
 
@@ -93,7 +103,6 @@ INSERT INTO `department` (`id`, `name`, `description`) VALUES
 
 CREATE TABLE `doctor` (
   `id` bigint(20) NOT NULL,
-  `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `firstName` varchar(255) DEFAULT NULL,
@@ -106,16 +115,29 @@ CREATE TABLE `doctor` (
 -- Déchargement des données de la table `doctor`
 --
 
-INSERT INTO `doctor` (`id`, `username`, `password`, `email`, `firstName`, `lastName`, `specialization`, `department_id`) VALUES
-(1, 'dr_john', 'password123', 'dr.john@example.com', 'John', 'Doe', 'Cardiologist', 1),
-(2, 'dr_jane', 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 1),
-(3, 'dr_jane', 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 1),
-(4, 'dr_jane', 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 1),
-(5, 'dr_jane', 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 1),
-(6, 'dr_jane', 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 1),
-(7, 'dr_jane', 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 1),
-(8, 'dr_jane', 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 1),
-(9, 'dr_jane', 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 1);
+INSERT INTO `doctor` (`id`, `password`, `email`, `firstName`, `lastName`, `specialization`, `department_id`) VALUES
+(2, 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 2),
+(3, 'password123', 'dr.jane@example.com', 'Jane', 'Smith', 'Neurologist', 2),
+(10, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(11, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(12, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(13, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(14, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(15, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(16, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(17, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(18, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(19, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(20, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(21, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(22, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(23, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(24, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(25, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(26, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(27, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(28, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1),
+(29, 'doctor123', 'doctor@example.com', 'John', 'Doe', 'Cardiology', 1);
 
 -- --------------------------------------------------------
 
@@ -125,7 +147,6 @@ INSERT INTO `doctor` (`id`, `username`, `password`, `email`, `firstName`, `lastN
 
 CREATE TABLE `patient` (
   `id` bigint(20) NOT NULL,
-  `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `firstName` varchar(255) DEFAULT NULL,
@@ -138,8 +159,21 @@ CREATE TABLE `patient` (
 -- Déchargement des données de la table `patient`
 --
 
-INSERT INTO `patient` (`id`, `username`, `password`, `email`, `firstName`, `lastName`, `phoneNumber`, `address`) VALUES
-(1, 'patient_1', 'password123', 'patient1@example.com', 'Jane', 'Doe', '123-456-7890', '456 Elm St');
+INSERT INTO `patient` (`id`, `password`, `email`, `firstName`, `lastName`, `phoneNumber`, `address`) VALUES
+(1, 'password123', 'patient1@example.com', 'Jane', 'Doe', '123-456-7890', '456 Elm St'),
+(2, 'mypassword', 'mike@example.com', 'Mike', 'Smith', NULL, 'Roued'),
+(3, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(4, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(5, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(6, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(7, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(8, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(9, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(10, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(11, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(12, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(13, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued'),
+(14, 'mypassword', 'youssef@example.com', 'Mike', 'Smith', '123456789', 'Roued');
 
 -- --------------------------------------------------------
 
@@ -154,13 +188,6 @@ CREATE TABLE `timeslot` (
   `isAvailable` tinyint(1) DEFAULT 1,
   `doctor_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `timeslot`
---
-
-INSERT INTO `timeslot` (`id`, `startTime`, `endTime`, `isAvailable`, `doctor_id`) VALUES
-(1, '2025-01-24 09:00:00', '2025-01-24 10:00:00', 1, 1);
 
 --
 -- Index pour les tables déchargées
@@ -221,25 +248,25 @@ ALTER TABLE `administrator`
 -- AUTO_INCREMENT pour la table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT pour la table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT pour la table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT pour la table `timeslot`
